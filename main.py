92250dafc628c1069e5d128c6f9df01b4a66e88a
Jonathan Riezman <riezman@gmail.com>
2017-09-18 17:35:53 -0500

    added link to vigenere, dummy page, and appropriate formatting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from caesar import rotate_string
+from caesar import rotate_string, alphabet_position
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,29 +22,39 @@ form = '''
                 width: 540px;
                 height: 120px;
             }}
+            h2 {{
+				margin: 0 auto;
+				text-align: center;
+			}}
         </style>
     </head>
     <body>
       <form method='POST'>
-		<label>Rotate By: 
+		<label>{1}: 
 		<input name="rot" type="text" value="0" />
 		</label>
 		<textarea name="text">{0}</textarea>
 		<input type="submit" value="Encrypt" />
       </form>
+      <br>
+      <h2>Caesar not secure enough? <a href="./vigenere">click here</a> for Vigenere Cipher instead.</h2>
     </body>
 </html>
 '''
 
 @app.route('/')
 def index():
-	return form.format('')
+	return form.format('','Rotate by')
 	
 @app.route('/', methods=['POST'])
 def encrypt():
 	rot = request.form['rot']
 	text = request.form['text']
 	message = rotate_string(text, rot)
-	return form.format(message) 
+	return form.format(message,'Rotate by')
 
+@app.route('/vigenere', methods=['POST'])
+def vigenere():
+	return "need to implement functionality"
+	
 app.run()
